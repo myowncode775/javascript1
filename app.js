3b417e10236136dc7ecfe0f9f4dc74e08e705905
jonix <almaget@yandex.com>
2025-01-28 00:26:24 -0800

    twelfth

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+let a = 1;
+console.log(a);
+a = 'test';
+console.log(a);
 
 
+// базовые арифмметические операторы
 
-console.log('hello world');
+const width = 50;
+const height = 3;
+const space = width * height;
+
+console.log(space);
+
+const newWidth = width / 33;
+console.log(newWidth);
+
+/*
+две
+строки
+*/
+
+const city = 'Moscow';
+const country = 'Russia';
+console.log(city + ', ' + country + ' ' + 5);
+
